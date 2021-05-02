6738884ffca6bda20625c7433fbc08d2125b7dd6
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2021-05-02 16:12:33 +0200

    Implemented search test

--- src/test/java/SoftwareTestingSearch.java
+++ src/test/java/SoftwareTestingSearch.java
@@ -1,12 +1,26 @@
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SoftwareTestingSearch {
+    private final String searchQuery = "Software Testing";
 
     @Test
     public void testSearch() {
         ChromeDriver driver = new ChromeDriver();
-        driver.get();
+        driver.get("https://google.com");
+
+        // Search for ”Software Testing”
+        WebElement searchElement = driver.findElementByName("q");
+        searchElement.sendKeys(searchQuery);
+        searchElement.submit();
+
+        // Verify that the title reflects the search query
+        String title = driver.getTitle();
+        assert(title.contains(searchQuery));
+
+        // Clean up
+        driver.close();
     }
 
 }
