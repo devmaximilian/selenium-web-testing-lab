cdb8261b416c6c88b494a7681b9e94b0489a8742
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2021-05-02 15:55:59 +0200

    Removed configuration script as downloading the jar files is redundant

--- src/test/java/SoftwareTestingSearch.java
+++ src/test/java/SoftwareTestingSearch.java
@@ -1,11 +1,12 @@
 import org.junit.jupiter.api.Test;
-
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SoftwareTestingSearch {
 
     @Test
     public void testSearch() {
-
+        ChromeDriver driver = new ChromeDriver();
+        driver.get();
     }
 
 }
